04f39f7699b0aa2a559a57998773eecaa3f8f230
Star-Sky-18 <1033690636@qq.com>
2020-01-13 21:50:19 +0800

    update

--- MyApplication/app/src/main/java/com/example/myapplication/OwnCourse.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication;
-
-import androidx.fragment.app.Fragment;
-
-public class OwnCourse extends Fragment {
-}
--- MyApplication/app/src/main/java/com/example/myapplication/Ref.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication;
-
-import androidx.fragment.app.Fragment;
-
-public class Ref extends Fragment {
-}
